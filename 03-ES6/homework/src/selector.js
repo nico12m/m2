9bbb5a48c6cd3abdf1968300208288f56519c120
nico12m <mojavik.nm@gmail.com>
2022-11-20 01:36:29 -0300

    terminado

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -18,7 +18,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if (selector.charAt(0)==="#")return "id";
+  if (selector.charAt(0)===".")return "class";
+  if (selector.includes("."))return "tag.class";
+  return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +33,20 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
+    matchFunction= (element)=> "#"+ element.id=== selector;
    
   } else if (selectorType === "class") {
+    matchFunction = (element)=> element.classList.contains(selector.slice(1));
     
   } else if (selectorType === "tag.class") {
+    matchFunction = (element)=>{
+    const [tagName,className] = element.split(".");
+    return matchFunctionMaker(tagName)(element) && matchFunctionMaker("."+ className)(element);
+    }
     
   } else if (selectorType === "tag") {
     
+    matchFunction = (element)=> element.tagName===selector.toUpperCase;
   }
   return matchFunction;
 };
